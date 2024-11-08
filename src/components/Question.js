d33dae90b22edf523ce32c2fdf17b6f4f2f98db7
hamzamoha-a <hamzashukri29@gmail.com>
2024-11-08 08:13:10 +0300

    first commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  
+  useEffect(() => {
+  
+    if (timeRemaining === 0) {
+      onAnswered(false); 
+      setTimeRemaining(10); 
+      return;
+    }
+
+    
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1); 
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]); 
 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
+    setTimeRemaining(10); 
+    onAnswered(isCorrect); 
   }
 
   const { id, prompt, answers, correctIndex } = question;
